9758fac1262b73a098560bd89c392bd5034ed4d4
cirynedh <148441958+cirynedh@users.noreply.github.com>
2025-02-06 10:17:00 -0800

    files added

---
+++ database.js
@@ -0,0 +1,32 @@
+const sqlite3 = require('sqlite3').verbose();
+
+// Connexion à la base de données SQLite
+const db = new sqlite3.Database('./maBaseDeDonnees.sqlite', sqlite3.OPEN_READWRITE |
+sqlite3.OPEN_CREATE, (err) => {
+if (err) {
+console.error(err.message);
+} else {
+console.log('Connecté à la base de données SQLite.');
+db.run(`CREATE TABLE IF NOT EXISTS personnes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom TEXT NOT NULL,
+    adresse TEXT
+    )`, (err) => {
+    if (err) {
+    console.error(err.message);
+    } else {
+// Insertion de données initiales avec adresses 
+const personnes = [
+    { nom: 'Bob', adresse: 'kairouan' },
+    { nom: 'Alice', adresse: 'soussse' },
+    { nom: 'Charlie', adresse: 'mahdia' }
+];
+personnes.forEach(({ nom, adresse }) => {
+    db.run(`INSERT INTO personnes (nom, adresse) VALUES (?, ?)`, [nom, adresse]);
+});
+}
+});
+}
+});
+
+module.exports = db;
